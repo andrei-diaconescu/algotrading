ff8cf0ed393b5ac47a471751341ab23f360cc078
andrei-diaconescu <andreidiaconescu_1999@yahoo.com>
2023-01-25 12:11:58 +0200

    update tests

--- tests/test_trade.py
+++ tests/test_trade.py
@@ -22,8 +22,6 @@ def test_peek_and_drawdown():
     def assert_peek_and_drawdown_prices_after_updating_trade_with_candle(
         expected_drawdown_price,
         expected_peek_price,
-        expected_drawdown_percentage,
-        expected_peek_percentage,
     ):
         trade.update_trade(candle)
         assert trade.drawdown_price == expected_drawdown_price
@@ -32,21 +30,17 @@ def test_peek_and_drawdown():
     candle = pd.DataFrame()
     candle.low = 99.8
     candle.high = 100
-    assert_peek_and_drawdown_prices_after_updating_trade_with_candle(99.8, 100, 0.2, 0)
+    assert_peek_and_drawdown_prices_after_updating_trade_with_candle(99.8, 100)
 
     candle.low = 99.3
     candle.high = 99.7
-    assert_peek_and_drawdown_prices_after_updating_trade_with_candle(99.3, 100, 0.7, 0)
+    assert_peek_and_drawdown_prices_after_updating_trade_with_candle(99.3, 100)
 
     candle.low = 99.1
     candle.high = 100.9
-    assert_peek_and_drawdown_prices_after_updating_trade_with_candle(
-        99.1, 100.9, 0.9, 0.9
-    )
+    assert_peek_and_drawdown_prices_after_updating_trade_with_candle(99.1, 100.9)
 
     # trigger SL
     candle.low = 98.3
     candle.high = 99.7
-    assert_peek_and_drawdown_prices_after_updating_trade_with_candle(
-        99.1, 100.9, 0.9, 0.9
-    )
+    assert_peek_and_drawdown_prices_after_updating_trade_with_candle(99.1, 100.9)
